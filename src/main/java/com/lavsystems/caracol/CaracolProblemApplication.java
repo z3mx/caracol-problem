97658f518c737a9ea6caec6490f2b1f899daa1ca
Luis B <z3mx@users.noreply.github.com>
2021-07-07 18:48:19 -0500

    Add files via upload

---
+++ src/main/java/com/lavsystems/caracol/CaracolProblemApplication.java
@@ -0,0 +1,14 @@
+package com.lavsystems.caracol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaracolProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaracolProblemApplication.class, args);
+	}
+
+
+}
